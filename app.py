d4e857831528d420d7ed53c428e8e98a3f73c034
wa86314 <2021wa86314@wilp.bits-pilani.ac.in>
2025-11-14 11:42:11 +0530

    Update app.py
    Code Changes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ fill_mask = pipeline("fill-mask", model="bert-base-uncased")
 
 @app.get("/")
 def root():
-    return {"status": "Canary service is up and running"}
+    return {"status": "<Canary> service is up and running"}
 
 @app.get("/predict",response_class=HTMLResponse)
 def predict(text: str):
